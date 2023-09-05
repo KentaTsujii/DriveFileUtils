95ce54963628681a848996d0a202eea3efa19ed6
Kenta Tsujii <kenta.tsujii@supership.jp>
2023-09-05 12:01:23 +0900

    add: fileutilのテスト関数を追加

--- DriveFileUtilsTest.ts
+++ DriveFileUtilsTest.ts
@@ -11,3 +11,27 @@ function folder_util_test_delete() {
 function folder_util_test_after() {
   FolderUtils.delete_dir('folder_utils_test');
 }
+
+function file_util_test(){
+  FolderUtils.create_dir('file_utils_test');
+  FileUtils.create_file('file_utils_test/test_file.txt', DocumentApp);
+  FileUtils.create_file('file_utils_test/test_file.xlsx', SpreadsheetApp);
+  try {
+    FileUtils.create_file('file_utils_test/test_file.xlsx', SpreadsheetApp);
+  } catch (error) {
+    console.warn(error) 
+  }
+  FileUtils.get_file('file_utils_test/test_file.txt', DocumentApp);
+  FileUtils.get_file('file_utils_test/test_file.xlsx', SpreadsheetApp);
+  FileUtils.get_or_create_file('file_utils_test/test_file.txt', DocumentApp);
+  FileUtils.get_or_create_file('file_utils_test/test_file2.xlsx', SpreadsheetApp);
+}
+
+function file_util_test_delete(){
+  FileUtils.delete_file('file_utils_test/test_file.txt');
+  FileUtils.delete_file('file_utils_test/test_file.xlsx');
+}
+
+function file_util_test_after() {
+  FolderUtils.delete_dir('file_utils_test');
+}
